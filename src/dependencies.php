e6c1541c89cd8f72f2fd5db30fb4dc60c26cf22b
segersniels <segers.n@hotmail.com>
2018-11-28 09:53:30 +0100

    clean up unused containers

--- src/dependencies.php
+++ src/dependencies.php
@@ -5,21 +5,6 @@ require __DIR__ . '/../includes/api.php';
 // DIC configuration
 $container = $app->getContainer();
 
-// view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
-};
-
-// monolog
-$container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-    return $logger;
-};
-
 // api
 $container['api'] = function ($c) {
     $settings = $c->get('settings');
